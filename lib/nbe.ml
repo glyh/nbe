62617e392d4820f82bd5125a6d5e20195e094348
glyh <lyhokia@gmail.com>
2024-09-09 10:44:47 +0800

    remove trash

--- lib/nbe.ml
+++
@@ -1,7 +0,0 @@
-(*open Fmlib_std.Result*)
-
-(*let alpha_equiv e1 e2 = *)
-(*  let alpha_equiv_aux e1 e2 syms1 syms2 =*)
-(*    match (e1, e2) with*)
-(*    | (Atom a1, Atom a2) -> 0 == compare a1 a2*)
-(*    | (Symbol sym1, Symbol sym2)*)
